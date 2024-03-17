f9b051f8e31d0184c28f9b9e57b3ed95e9ff5624
Muhammad Sohail <msohailshahbaz058@gmail.com>
2024-03-17 12:53:04 +0500

    Login Feature

--- FYPro/Server/Controllers/AuthController.cs
+++ FYPro/Server/Controllers/AuthController.cs
@@ -15,6 +15,7 @@ using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Data;
+using FYPro.Shared;
 
 
 namespace FYPro.Server.Controllers
@@ -35,29 +36,44 @@ namespace FYPro.Server.Controllers
             SqlConnection con = new SqlConnection(configuration.GetConnectionString("Default"));
             return con;
         }
-        private async Task<string> CreateJWT(string Username)
-        {
-            var secretkey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration["jwt:Key"]));
-            var credentials = new SigningCredentials(secretkey, SecurityAlgorithms.HmacSha256);
-            String Roles;
-            if (Char.IsDigit(Username[0]))
-            {
-                Roles = "student";
-            }
-            else
-            {
-                Roles = "faculty";
-            }
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.Name, Username),
-                new Claim(ClaimTypes.Role, Roles),
-                new Claim(JwtRegisteredClaimNames.Sub, Username),
-                new Claim(JwtRegisteredClaimNames.Jti, Username)
-            };
+        //private async Task<string> CreateJWT(string Username)
+        //{
+        //    var secretkey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration["jwt:Key"]));
+        //    var credentials = new SigningCredentials(secretkey, SecurityAlgorithms.HmacSha256);
+        //    String Roles;
+        //    if (Char.IsDigit(Username[0]))
+        //    {
+        //        Roles = "student";
+        //    }
+        //    else
+        //    {
+        //        Roles = "faculty";
+        //    }
+        //    var claims = new[]
+        //    {
+        //        new Claim(ClaimTypes.Name, Username),
+        //        new Claim(ClaimTypes.Role, Roles),
+        //        new Claim(JwtRegisteredClaimNames.Sub, Username),
+        //        new Claim(JwtRegisteredClaimNames.Jti, Username)
+        //    };
 
-            var token = new JwtSecurityToken(issuer: configuration["jwt:Issuer"], audience: configuration["jwt:Audience"], claims: claims, expires: DateTime.Now.AddMonths(1), signingCredentials: credentials);
-            return new JwtSecurityTokenHandler().WriteToken(token);
+        //    var token = new JwtSecurityToken(issuer: configuration["jwt:Issuer"], audience: configuration["jwt:Audience"], claims: claims, expires: DateTime.Now.AddMonths(1), signingCredentials: credentials);
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
+
+        [HttpGet("VerificationUser/{Email}")]
+        public async Task<ActionResult<List<int>>> VerifyExistenceStudent(string Email)
+        {
+            var i = await CreateConnection().QueryAsync<int>($"select count(*) Count from Users where Email ='{Email}'");
+            return Ok(i);
+        }
+        [HttpGet("user/{Email}")]
+        public async Task<ActionResult<List<LoginModel>>> VerifyUser(string Email)
+        {
+
+            var i = await CreateConnection().QueryAsync<LoginModel>($"SELECT * FROM Users WHERE Email = '{Email}'");
+            //i.ToList()[0].jwtbearer = await CreateJWT(Email);
+            return Ok(i);
         }
     }
 }
