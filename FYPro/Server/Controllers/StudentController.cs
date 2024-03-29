b2da5b7775918c0c314cfcad09fe4412b9ae119d
Muhammad Sohail <msohailshahbaz058@gmail.com>
2024-03-30 01:59:17 +0500

    Student Homepage completed

---
+++ FYPro/Server/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Dapper;
+using System.Security.Cryptography;
+using System.Text;
+using System.Numerics;
+using System.Xml.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Data;
+using FYPro.Shared;
+
+
+namespace FYPro.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public IConfiguration configuration;
+
+        public StudentController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            SqlConnection con = new SqlConnection(configuration.GetConnectionString("Default"));
+            return con;
+        }
+
+
+        [HttpGet("GetStudentInfo/{Email}")]
+        public async Task<ActionResult<List<StudentModel>>> GetStudentInfo(string Email)
+        {
+            var i = await CreateConnection().QueryAsync<StudentModel>($"Select FirstName,LastName,Email,RollNumber,Degree,Program,Campus,PhoneNumber,ParentsPhoneNumber,DOB,CNIC from Students \nJoin Users on Students.UserID = Users.UserID \nwhere Email = '{Email}'");
+            return Ok(i);
+        }
+    }
+}
+
---
+++ FYPro/Shared/StudentModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FYPro.Shared;
+
+public class StudentModel
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string RollNumber { get; set; } = string.Empty;
+    public string Degree { get; set; } = string.Empty;
+    public string Program { get; set; } = string.Empty;
+    public string Campus { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string ParentsPhoneNumber { get; set; } = string.Empty;
+    public string DOB { get; set; } = string.Empty;
+    public string CNIC { get; set; } = string.Empty;
+
+}
+
