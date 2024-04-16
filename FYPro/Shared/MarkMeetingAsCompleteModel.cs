2c026b965daa005c6e89323834cab8258b9603e5
Muhammad Sohail <msohailshahbaz058@gmail.com>
2024-04-16 13:25:55 +0500

    Schedule Meeting Complete

---
+++ FYPro/Shared/MarkMeetingAsCompleteModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FYPro.Shared;
+
+public class ViewMeetingModel
+{
+    public string MeetingID { get; set; } = string.Empty;
+    public string ProjectName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime MeetingDateTime { get; set; }  
+    public string Agenda { get; set; } = string.Empty;
+    public string Complete { get; set; } = string.Empty;
+}
+
+
