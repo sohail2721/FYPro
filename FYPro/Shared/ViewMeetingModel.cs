aca63852e1df9b96e9c4bef8caeaba42f7f0a02f
Muhammad Sohail <msohailshahbaz058@gmail.com>
2024-04-16 11:46:32 +0500

    Student User can now view scheduled meetings(Mark as complete feature remaining)

---
+++ FYPro/Shared/ViewMeetingModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FYPro.Shared;
+
+public class MeetingModel
+{
+    public string ProjectID { get; set; } = string.Empty;
+    public string SupervisorFacultyNumber { get; set; } = string.Empty;
+    public string RollNumber { get; set; } = string.Empty;
+    public DateTime MeetingDateTime { get; set; }  
+    public string Agenda { get; set; } = string.Empty;
+}
+
